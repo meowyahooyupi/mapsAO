1f2a4fca4faa8aa595672f27b912d44ebe6c149b
meowy <ilikeitmmmmmmmmmmmmmmmmm@gmail.com>
2025-12-24 19:08:45 -0300

    mobile zoom adjustment 2

--- source.js
+++ source.js
@@ -663,7 +663,7 @@ containerOverflow.ontouchmove = (event) => {
         return
     }
     let distScale = calculateTouchDist(event.touches)/touchBeganDist
-    setScale(touchBeganScale+1*distScale)
+    setScale(touchBeganScale+(distScale-1))
     event.preventDefault()
 }
 containerOverflow.ontouchend = () => {
